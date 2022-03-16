f4635414de1d3c5995941dcc1ab3c50118de7509
makaumun <makaumun@gmail.com>
2022-03-16 12:21:14 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,66 +1,47 @@
+#include <stdio.h>
 #include "main.h"
 
-int _putchar(char c);
-
 /**
- * print_times_table - print the times table until n
+ * print_times_table - Prints the n times table
  *
- * @n: n times table
+ * @n: number times table (0 < n <= 15)
  *
- * Return: No return
+ * Return: no return
  */
-
-void times_table(int n)
+void print_times_table(int n)
 {
-	int i;
-	int j;
+	int a, b, op;
 
 	if (n >= 0 && n <= 15)
 	{
-
-		for (i = 0; i <= n; i++)
+		for (a = 0; a <= n; a++)
 		{
-
-			for (j = 0; j <= n; j++)
+			_putchar(48);
+			for (b = 1; b <= n; b++)
 			{
-
-				if ((i * j) < 10)
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (op <= 9)
 				{
-					if (j != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar((j * i) + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
 				}
-
-				else if ((i * j) < 100)
+				else if (op <= 99)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((((j * i) / 10) % 10) + '0');
-					_putchar(((j * i) % 10) + '0');
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
 				}
-
-				else if ((i * j) < 1000)
+				else
 				{
-					_putchar(' ');
-					_putchar((((j * i) / 100) % 10) + '0');
-					_putchar((((j * i) / 10) % 10) + '0');
-					_putchar(((j * i) % 10) + '0');
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
 				}
-			if (j != (n))
-			{
-				_putchar(',');
 			}
-
-			}
-
-		_putchar('\n');
-
+			_putchar('\n');
 		}
-
 	}
-
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -7,6 +9,8 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(int n);
-int add(int, int);
+void times_table(void);
+int add (int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+void putformat(int);
